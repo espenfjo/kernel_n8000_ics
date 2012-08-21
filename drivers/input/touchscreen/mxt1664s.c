b98acda09d70ec35a23a396b6113528d1f573c16
Andrew Dodd <atd7@cornell.edu>
2012-08-20 21:04:14 -0400

    mxt1664s: Disable "Unknown state" messages for now
    There are lots of these and they spam the log, but
    TS seems to be working fine.
    
    Signed-off-by: Andrew Dodd <atd7@cornell.edu>

--- drivers/input/touchscreen/mxt1664s.c
+++ drivers/input/touchscreen/mxt1664s.c
@@ -843,7 +843,7 @@ static void mxt_treat_T9_object(struct mxt_data *data, u8 *msg)
 		/* ignore changed amplitude message */
 		if (!((msg[1] & DETECT_MSG_MASK)
 				&& (msg[1] & AMPLITUDE_MSG_MASK)))
-			dev_err(&data->client->dev, "Unknown state %#02x %#02x\n",
+			dev_dbg(&data->client->dev, "Unknown state %#02x %#02x\n",
 				msg[0], msg[1]);
 	}
 }
